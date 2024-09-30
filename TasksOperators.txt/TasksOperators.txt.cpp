e3ef7d5fd924a1e170f0f38905e4e1c08cdb9947
KleversDS <klevers170@gmail.com>
2024-09-30 23:49:17 +0300

    Добавьте файлы проекта.

---
+++ TasksOperators.txt/TasksOperators.txt.cpp
@@ -0,0 +1,149 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {//1
+    //int minutes;
+    //cout << "Enter the number of minutes since the start of the week: ";
+    //cin >> minutes;
+
+    //int total_minutes_in_week = 7 * 24 * 60;
+    //int remaining_minutes = total_minutes_in_week - minutes;
+
+    //int days = minutes / (24 * 60);
+    //int hours = (minutes % (24 * 60)) / 60;
+    //int mins = minutes % 60;
+    //int seconds = 0;
+
+    //int remaining_days = remaining_minutes / (24 * 60);
+    //int remaining_hours = (remaining_minutes % (24 * 60)) / 60;
+    //int remaining_mins = remaining_minutes % 60;
+
+    //cout << "Current day: " << days + 1 << ", hour: " << hours << ", minutes: " << mins << ", seconds: " << seconds << endl;
+    //cout << "Time left until the end of the week: " << remaining_days << " days, " << remaining_hours << " hours, " << remaining_mins << " minutes.\n";
+   
+    //}
+////2
+/*    double length, width;
+    cout << "Enter the length of the rectangle: ";
+    cin >> length;
+    cout << "Enter the width of the rectangle: ";
+    cin >> width;
+
+    double area = length * width;
+    double perimeter = 2 * (length + width);
+
+    cout << "Area of the rectangle: " << area << endl;
+    cout << "Perimeter of the rectangle: " << perimeter << endl;
+       
+      */  
+////3
+//    double a, b, c;
+//    cout << "Enter the three sides of the triangle: ";
+//    cin >> a >> b >> c;
+//
+//    double perimeter = a + b + c;
+//    double s = perimeter / 2;
+//    double area = sqrt(s * (s - a) * (s - b) * (s - c));
+//
+//    cout << "Perimeter of the triangle: " << perimeter << endl;
+//    cout << "Area of the triangle: " << area << endl;
+//      
+//            
+////4     
+//    double weight, price;
+//    int quantity;
+//
+//    cout << "Enter the weight of one item (kg): ";
+//    cin >> weight;
+//    cout << "Enter the quantity of items: ";
+//    cin >> quantity;
+//    cout << "Enter the price per item: ";
+//    cin >> price;
+//
+//    double total_weight = weight * quantity;
+//    double total_cost = price * quantity;
+//
+//    cout << "Total weight of the order: " << total_weight << " kg" << endl;
+//    cout << "Total cost of the order: $" << total_cost << endl;
+//
+////5
+// double price_per_kg, quantity;
+//    cout << "Enter the price per kilogram of apples: ";
+//    cin >> price_per_kg;
+//    cout << "Enter the quantity of apples (kg): ";
+//    cin >> quantity;
+//
+//    double total_cost = price_per_kg * quantity;
+//
+//     if (quantity > 5) {
+//        total_cost *= 0.9;
+//     }
+//
+//    cout << "Total cost of the apples: $" << total_cost << endl;
+//
+////6
+//    double distance, speed;
+//    cout << "Enter the distance between the cities (km): ";
+//    cin >> distance;
+//    cout << "Enter the speed of the car (km/h): ";
+//    cin >> speed;
+//
+//    double travel_time = distance / speed;
+//    double time_day = 24 - (8 + travel_time);
+//
+//    cout << "Travel time: " << time << " hours" << endl;
+//    cout << "Time left in the day after the trip: " << time_day << " hours" << endl;
+//
+////7
+//   double price_per_book, tax_rate;
+//   int quantity;
+//
+//    cout << "Enter the price per book: ";
+//    cin >> price_per_book;
+//    cout << "Enter the number of books: ";
+//    cin >> quantity;
+//    cout << "Enter the VAT percentage: ";
+//    cin >> tax_rate;
+//    double total_cost = price_per_book * quantity;
+//    double final_cost = total_cost + (total_cost * tax_rate / 100);
+//    cout << "Total cost including VAT: $" << final_cost << endl;
+//
+//
+////8
+//    
+//    double water_volume, price_per_cubic_meter;
+//    cout << "Enter the volume of water used in liters: ";
+//    cin >> water_volume;
+//    cout << "Enter the price per cubic meter of water: ";
+//    cin >> price_per_cubic_meter;
+//
+//    double cubic_meters = water_volume / 1000;
+//    double total_cost = cubic_meters * price_per_cubic_meter;
+//
+//    cout << "Total cost for water: $" << total_cost << endl;
+//
+//
+////9
+//    double file_size, write_speed;
+//    cout << "Enter the file size in bytes: ";
+//    cin >> file_size;
+//    cout << "Enter the write speed in megabytes per second (MB/s): ";
+//    cin >> write_speed;
+//    double file_size_mb = file_size / (1024 * 1024);
+//    double write_time = file_size_mb / write_speed;
+//    cout << "Time to write the file: " << write_time << " seconds" << endl;
+//
+//
+////10
+//    double power, hours_per_day;
+//    cout << "Enter the power of the device in watts: ";
+//    cin >> power;
+//    cout << "Enter the number of hours per day the device is used: ";
+//    cin >> hours_per_day;
+//
+//    double daily_consumption = (power / 1000) * hours_per_day;
+//    double monthly_consumption = daily_consumption * 30;
+//
+//    cout << "Daily consumption: " << daily_consumption << " kWh" << endl;
+//    cout << "Monthly consumption: " << monthly_consumption << " kWh" << endl;
+}
